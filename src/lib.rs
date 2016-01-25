239ad026ba7993972f81cff775198c33e6306b08
Boris Faure <billiob@gmail.com>
2016-01-25 21:33:58 +0100

    add test showing issue with files that have ≤ 2 colors
    The file has been tested with "identify" from imagemagick and "gifsicle".
    The issue is that the code size for the LZW compression is not correctly
    set for frames with only 2 colors.

--- src/lib.rs
+++ src/lib.rs
@@ -149,15 +149,19 @@ pub use encoder::{Encoder, ExtensionData, Repeat};
 fn round_trip() {
 	use std::io::prelude::*;
 	use std::fs::File;
-	let mut data = vec![];
-	File::open("tests/samples/sample_1.gif").unwrap().read_to_end(&mut data).unwrap();
-	let mut decoder = Decoder::new(&*data).read_info().unwrap();
-	let palette: Vec<u8> = decoder.palette().unwrap().into();
-	let frame = decoder.read_next_frame().unwrap().unwrap();
-	let mut data2 = vec![];
-	{
-    	let mut encoder = Encoder::new(&mut data2, frame.width, frame.height, &palette).unwrap();
-		encoder.write_frame(frame).unwrap();
+	let files = vec!["tests/samples/sample_1.gif",
+	                 "tests/samples/sample_2.gif"];
+	for file in files {
+		let mut data = vec![];
+		File::open(file).unwrap().read_to_end(&mut data).unwrap();
+		let mut decoder = Decoder::new(&*data).read_info().unwrap();
+		let palette: Vec<u8> = decoder.palette().unwrap().into();
+		let frame = decoder.read_next_frame().unwrap().unwrap();
+		let mut data2 = vec![];
+		{
+			let mut encoder = Encoder::new(&mut data2, frame.width, frame.height, &palette).unwrap();
+			encoder.write_frame(frame).unwrap();
+		}
+		assert_eq!(&data[..], &data2[..])
 	}
-	assert_eq!(&data[..], &data2[..])
 }
