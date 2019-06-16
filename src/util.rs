a96be35f04f3a421c260b4bd20153e5eff454334
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2019-06-16 21:27:28 +0200

    Replace unsafe util function with a safe equivalent that has been stabilized in 1.0.9. Even Debian Oldstable ships a newer version at this point.

--- src/util.rs
+++ src/util.rs
@@ -1,18 +1,13 @@
 //! Utility functions
 
-// Copies data from `src` to `dst`
-//
-// Panics if the length of `dst` is less than the length of `src`.
-// NOTE: this is a copy-paste of the unstable function `std::slice::bytes::copy_memory`.
+/// Copies data from `src` to `dst`, but does not guarantee it will fill `dst`
+///
+/// Unlike slice::copy_from_slice(), this function does not guarantee
+/// that `dst` will be completely overwritten:
+/// `src` is allowed to be smaller than `dst`, and in that case
+/// only `src.len()` bytes will be copied.
 #[inline]
 pub fn copy_memory(src: &[u8], dst: &mut [u8]) {
-    let len_src = src.len();
-    assert!(dst.len() >= len_src);
-    // `dst` is unaliasable, so we know statically it doesn't overlap
-    // with `src`.
-    unsafe {
-        ::std::ptr::copy_nonoverlapping(src.as_ptr(),
-                                      dst.as_mut_ptr(),
-                                      len_src);
-    }
+    let destination = &mut dst[..src.len()];
+    destination.copy_from_slice(src);
 }
