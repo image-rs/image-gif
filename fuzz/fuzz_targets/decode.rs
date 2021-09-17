28199644ee991d54d27874e5ccf2a712eeda1dd4
Andreas Molzer <andreas.molzer@gmx.de>
2021-09-17 23:37:05 +0200

    Add fuzz code for decode

--- fuzz/fuzz_targets/decode.rs
+++ fuzz/fuzz_targets/decode.rs
@@ -2,5 +2,13 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
+    let _ = try_decode(data);
 });
+
+fn try_decode(data: &[u8]) -> Result<(), gif::DecodingError> {
+    let mut reader = gif::Decoder::new(data)?;
+
+    while let Some(_) = reader.read_next_frame()? {}
+
+    Ok(())
+}
